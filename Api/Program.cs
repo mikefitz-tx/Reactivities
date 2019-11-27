d857715d2a82038e79485cb77eb03b145e0f143d
Mike Fitzgibbons <Mike.Fitzgibbons@contractor.conocophillips.com>
2019-11-27 14:28:20 -0600

    Next checkin

--- Api/Program.cs
+++ Api/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace Api
 {
@@ -13,7 +16,21 @@ namespace Api
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            
+            var host = CreateWebHostBuilder(args).Build();
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                }
+                catch(Exception ex) {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred during migration");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateWebHostBuilder(string[] args) =>
